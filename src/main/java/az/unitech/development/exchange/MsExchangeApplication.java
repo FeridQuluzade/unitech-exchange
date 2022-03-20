a61a58b8e92b81d6a59e0078ea188571bac16b2f
FeridQuluzade <feridquluzade2002@gmail.com>
2022-03-20 22:24:45 +0400

    initial commit

---
+++ src/main/java/az/unitech/development/exchange/MsExchangeApplication.java
@@ -0,0 +1,13 @@
+package az.unitech.development.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsExchangeApplication.class, args);
+    }
+
+}
